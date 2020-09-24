6928c3a38fc78d2e9a69ae3a0645b8804479f48f
George-debug <george.butco@gmail.com>
2020-09-24 15:11:36 +0300

    first commit

---
+++ precache-manifest.77ae8457d13878a0f2e5a9ca528482b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ab3c91e2d960a8684fd7cb02314e54",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a8b27be5f8104d00585",
+    "url": "./static/css/main.1e4815b2.chunk.css"
+  },
+  {
+    "revision": "bd8d025b84e5e9d5e250",
+    "url": "./static/js/2.3296234d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.3296234d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8b27be5f8104d00585",
+    "url": "./static/js/main.a2d870bb.chunk.js"
+  },
+  {
+    "revision": "d9b32a6b0fb9ccf86265",
+    "url": "./static/js/runtime-main.50d56654.js"
+  }
+]);
